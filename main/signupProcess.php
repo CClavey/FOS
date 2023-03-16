b49af5d4011add1191c19bbfccccd1474661243b
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-03-16 00:37:07 -0500

    Add files via upload

---
+++ main/signupProcess.php
@@ -0,0 +1,24 @@
+<?php
+
+include("header.php");
+include("orderHeader.php");
+
+include("fos-db-connection.php");
+
+$emailID = $_POST['emailID'];
+$pwd = $_POST['pwd'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$gender = $_POST['gender'];
+$dob = $_POST['dob'];
+
+$sql = "INSERT INTO users_tab (emailID, firstname, lastname, gender, pwd, dob) 
+		VALUES ('$emailID', '$firstname', '$lastname', '$gender', '$pwd', '$dob')";
+$result = $connect->query($sql);
+
+if($result == FALSE){
+	echo "error:".$connect->error;
+} else {
+	echo "<br><br><br><center>User successfully added</center>";
+}
+?>
