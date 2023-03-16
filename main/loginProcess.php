1ba7913ecef99960a046424566eb6d32bb1e7bc3
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-03-16 00:36:04 -0500

    Update loginProcess.php

--- main/loginProcess.php
+++ main/loginProcess.php
@@ -5,10 +5,10 @@ include("orderHeader.php");
 
 include("fos-db-connection.php");
 
-$userid = $_POST['emailID'];
+$emailID = $_POST['emailID'];
 $password = $_POST['pwd'];
 
-$sql = "SELECT pwd FROM users_tab WHERE emailID = '".$userid."'"; //this is susceptible to SQL injection, but i forgot how to do safe queries
+$sql = "SELECT pwd FROM users_tab WHERE emailID = '".$emailID."'"; //this is susceptible to SQL injection, but i forgot how to do safe queries
 $result = $connect->query($sql);
 $pwd = mysqli_fetch_assoc($result);
 
