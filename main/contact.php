50aeb42a9f58789e64339c321b7588e0bce483a9
CClavey <116412808+CClavey@users.noreply.github.com>
2023-03-16 10:08:40 -0500

    Update contact.php

--- main/contact.php
+++ main/contact.php
@@ -18,15 +18,15 @@
 					<td colspan="2" class = "tdMain">
 						<h4>First Name&ensp;&ensp;&ensp;<input type="text" name="firstname" id="firstname" value = ""/></h4>
 						<h4>Last Name&ensp;&ensp;&ensp;<input type="text" name="lastname" id="lastname" value = ""/></h4>
-						<h4>Email Address&ensp;&ensp;&ensp;<input type="text" name="emailID" id="emailID" value = ""/></h4>
-						<h4>Phone&ensp;&ensp;&ensp;<input type="text" name="phoneNum" id="phoneNum" value = ""/></h4>
+						<h4>Email Address&ensp;&ensp;&ensp;<label for="email"></label> <input type="email" id="emailID" name="emailID" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="example@email.com"></h4>
+						<h4>Phone&ensp;&ensp;&ensp;<input type="tel" name="phoneNum" id="phoneNum" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value = ""/></h4>
 						<h4>Contact Reason&ensp;&ensp;&ensp;<select id = "reason" name = "reason" value = "reason">
 							<option value = "Feedback">Feedback</option>
 							<option value = "Complaint">Complaint</option>
 							<option value = "Customer Support">Customer Support</option>
 						</select></h4>
 						<h4>Feedback&ensp;&ensp;&ensp;<textarea style="width:150px;height:100px;" name="feedback" id="feedback" value = ""></textarea></h4>
-						&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input type="submit" value="Submit">
+						&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input type="submit" name = "Submit" id = "Submit" value="Submit">
 					</td>
 				</tr>
 				<tr>
