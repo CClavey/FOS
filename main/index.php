945f332f1b05877f2e135863cef4827097f4f608
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-03-15 23:38:19 -0500

    Update index.php

--- main/index.php
+++ main/index.php
@@ -4,7 +4,7 @@
 	<link rel="stylesheet" href="CSS/styles.css">
 	<script src="JS/scripts.js"></script>
 	<center>
-		<a href = "login.php" class = "drp"><button style="width: 200px; height: 65px; font-size: 24px;" name = "loginOption" id = "loginOption" onclick = "login()">Login</button></a>
+		<a href = "login.php?login=''" class = "drp"><button style="width: 200px; height: 65px; font-size: 24px;" name = "loginOption" id = "loginOption" onclick = "login()">Login</button></a>
 		<a href = "signup.php" class = "drp"><button style="width: 200px; height: 65px; font-size: 24px;" name = "signupOption" id = "signupOption" onclick = "signup()">Sign-Up</button></a>
 		<br>
 		<br>
