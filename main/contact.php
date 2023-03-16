5c49b891e16f9c41c7bcfff23c56c1caee79bb19
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-03-16 01:51:04 -0500

    Update contact.php

--- main/contact.php
+++ main/contact.php
@@ -7,33 +7,35 @@
 
 <body class = "mainBody">
 	<center>
-		<table class="mainTable">
-			<tr>
-				<td colspan="2">
-					<hr>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2" class = "tdMain">
-					<h4>First Name&ensp;&ensp;&ensp;<input type="text" name="fName" id="fName" value = ""/></h4>
-					<h4>Last Name&ensp;&ensp;&ensp;<input type="text" name="lName" id="lName" value = ""/></h4>
-					<h4>Email Address&ensp;&ensp;&ensp;<input type="text" name="emailA" id="emailA" value = ""/></h4>
-					<h4>Phone&ensp;&ensp;&ensp;<input type="text" name="emailA" id="emailA" value = ""/></h4>
-					<h4>Contact Reason&ensp;&ensp;&ensp;<select id = "reason" name = "reason" value = "reason">
-						<option value = "Feedback">Feedback</option>
-						<option value = "Complaint">Complaint</option>
-						<option value = "Customer Support">Customer Support</option>
-					</select></h4>
-					<h4>Phone&ensp;&ensp;&ensp;<input type="textarea" name="emailA" id="emailA" value = ""/></h4>
-					<h4>Checkout&ensp;&ensp;&ensp;<a href = "contactProcess.php" class = "drp"><input type = "submit" name = "Submit" id = "Submit"></a></h4>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2">
-					<hr>
-				</td>
-			</tr>
-		</table>
+		<form name="contact-form" method="POST" action="contactProcess.php">
+			<table class="mainTable">
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" class = "tdMain">
+						<h4>First Name&ensp;&ensp;&ensp;<input type="text" name="firstname" id="firstname" value = ""/></h4>
+						<h4>Last Name&ensp;&ensp;&ensp;<input type="text" name="lastname" id="lastname" value = ""/></h4>
+						<h4>Email Address&ensp;&ensp;&ensp;<input type="text" name="emailID" id="emailID" value = ""/></h4>
+						<h4>Phone&ensp;&ensp;&ensp;<input type="text" name="phoneNum" id="phoneNum" value = ""/></h4>
+						<h4>Contact Reason&ensp;&ensp;&ensp;<select id = "reason" name = "reason" value = "reason">
+							<option value = "Feedback">Feedback</option>
+							<option value = "Complaint">Complaint</option>
+							<option value = "Customer Support">Customer Support</option>
+						</select></h4>
+						<h4>Feedback&ensp;&ensp;&ensp;<input type="textarea" name="feedback" id="feedback" value = ""/></h4>
+						&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input type="submit" value="Submit">
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+			</table>
+		</form>
 	</center>
 </body>
 
