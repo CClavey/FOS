e2ff8aff5077e2b20727426e05c97fa6e9189853
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-03-15 23:37:59 -0500

    Update loginProcess.php

--- main/loginProcess.php
+++ main/loginProcess.php
@@ -1,9 +1,21 @@
 <?php
 
+include("header.php");
+include("orderHeader.php");
+
+include("fos-db-connection.php");
+
 $userid = $_POST['emailID'];
 $password = $_POST['pwd'];
 
-echo $userid;
-echo $password;
+$sql = "SELECT pwd FROM users_tab WHERE emailID = '".$userid."'"; //this is susceptible to SQL injection, but i forgot how to do safe queries
+$result = $connect->query($sql);
+$pwd = mysqli_fetch_assoc($result);
 
+if($password == $pwd['pwd']) {
+	echo "<br><br><br><center>Login Successful</center>";
+} else {
+	header("Location: login.php?login=fail");
+	exit();
+}
 ?>
