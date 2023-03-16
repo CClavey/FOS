6bd05f4124742e9f3f0b0b78b2cd9922ddee9412
CClavey <116412808+CClavey@users.noreply.github.com>
2023-03-15 21:35:33 -0500

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,13 @@
 function signup()
 {
-	reset()
+	reset();
 	document.getElementById("signupDIV").style.display = "block";
 	document.getElementById("loginDIV").style.display = "none";
 }
 
 function login()
 {
-	reset()
+	reset();
 	document.getElementById("loginDIV").style.display = "block";
 	document.getElementById("signupDIV").style.display = "none";
 }
@@ -20,70 +20,6 @@ function passwordCheck()
 		var genderVar = document.getElementById("gender").value
 		var DOBVar = document.getElementById("dob").value
 		var emailIDVar = document.getElementById("emailID").value
-		alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nGender: " + genderVar + "\nDOB: " + DOBVar + "\nEmail ID: " + emailIDVar)
-		if (document.getElementById("HS").checked)
-		{
-			alert("You have a highschool diploma.")
-		}
-		if (document.getElementById("BS").checked)
-		{
-			alert("You have a Bachelor's Degree.")
-		}
-		if (document.getElementById("MS").checked)
-		{
-			alert("You have a Master's Degree.")
-		}
-		if (document.getElementById("PhD").checked)
-		{
-			alert("You have a PhD.")
-		}
-		if (document.getElementById("Cybersecurity").checked)
-		{
-			alert("You are in Cybersecurity.")
-		}
-		else if (document.getElementById("WebDesign").checked)
-		{
-			alert("You are in Web Design.")
-		}
-		else if (document.getElementById("Electrical").checked)
-		{
-			alert("You are in Electrical Engineering.")
-		}
-		else if (document.getElementById("Mechanical").checked)
-		{
-			alert("You are in Mechanical Engineering.")
-		}
-		else if (document.getElementById("other").checked)
-		{
-			var otherMajor = document.getElementById("otherField").value;
-			alert("You are in " + otherMajor + ".")
-		}
-		
-		if (document.getElementById("C++").checked)
-		{
-			alert("You know C++.")
-		}
-		if (document.getElementById("Python").checked)
-		{
-			alert("You know Python.")
-		}
-		if (document.getElementById("C#").checked)
-		{
-			alert("You know C#.")
-		}
-		if (document.getElementById("HTML").checked)
-		{
-			alert("You know HTML.")
-		}
-		if (document.getElementById("Java").checked)
-		{
-			alert("You know Java.")
-		}
-		if (document.getElementById("SQL").checked)
-		{
-			alert("You know SQL.")
-		}
-		
 	}
 	else
 	{
@@ -97,41 +33,11 @@ function reset()
 	document.getElementById("lastname").value = "";
 	document.getElementById("gender").value = "";
 	document.getElementById("dob").value = "";
-	document.getElementById("HS").checked = false;
-	document.getElementById("BS").checked = false;
-	document.getElementById("MS").checked = false;
-	document.getElementById("PhD").checked = false;
-	document.getElementById("Cybersecurity").checked = false;
-	document.getElementById("WebDesign").checked = false;
-	document.getElementById("Electrical").checked = false;
-	document.getElementById("Mechanical").checked = false;
-	document.getElementById("C++").checked = false;
-	document.getElementById("Python").checked = false;
-	document.getElementById("C#").checked = false;
-	document.getElementById("HTML").checked = false;
-	document.getElementById("Java").checked = false;
-	document.getElementById("SQL").checked = false;
 	document.getElementById("emailID").value = "";
 	document.getElementById("password").value = "";
 	document.getElementById("confirm").value = "";
-	document.getElementById("userid").value = "";
+	document.getElementById("emailID2").value = "";
 	document.getElementById("pwd").value = "";
-	document.getElementById("other").checked = false;
-	document.getElementById("otherField").style.display = "none";
-	document.getElementById("otherField").value = "";
-}
-
-function other()
-{
-	var box = document.getElementById("other");
-	if (box.checked == true)
-	{
-		document.getElementById("otherField").style.display = "block";
-	}
-	else
-	{
-		document.getElementById("otherField").style.display = "none";
-	}
 }
 
 function display()
