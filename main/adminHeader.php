f7f38a539ceb9e20cf687428ee653fe42c0a4878
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-04-25 01:22:01 -0500

    Create adminHeader.php

---
+++ main/adminHeader.php
@@ -0,0 +1,25 @@
+<html>
+<header>
+<link rel="stylesheet" href="CSS/styles.css">
+<script src="JS/scripts.js"></script>
+<center>
+	<ul style="background-color:#f13e36;float:right">
+		
+		<!--<li class="menu-item"><a href="adminDeals.php" class="drp">Deals</a>
+		</li>
+		
+		<li class="menu-item"><a href="adminPizzas.php" class="drp">Pizzas</a>
+		</li>
+		
+		<li class="menu-item"><a href="adminExtras.php" class="drp">Extras</a>
+		</li>
+		
+		<li class="menu-item"><a href="adminOrders.php" class="drp">Orders</a>
+		</li>-->
+		
+		<li class='menu-item'><a href='index.php' class='drp'>Logout<img src = 'Images/logout.png' height = '20px' width = '20px'></a>
+					</li>
+	</ul>
+</center>
+</header>
+</html>
