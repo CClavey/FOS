610cc47626357a2d732348f9eda6128944f7f5fd
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-04-25 01:21:26 -0500

    Update accountDetails.php

--- main/accountDetails.php
+++ main/accountDetails.php
@@ -1,15 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html lang="en-US">
 <header>
 <?php include ("header.php") ?>
-<!-- Need to add session variables so the USER ID can move across the files -->
 <link rel="stylesheet" href="CSS/styles.css">
 <script src="scripts.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 </header>
-<!INCLUDE A SCRIPT TO AUTOMATICALLY PUT CURRENT INFO INTO THE FIELDS SO A USER DOES NOT HAVE TO RE-ENTER ALL OF THEIR INFO>
 
 <center>
 <body class = "mainBody">
-<table class = "mainTable">
+
+<div ng-app="myApp" ng-controller="myCtrl">
+<form name="account-form" method="POST" action="detailsProcess.php" >
+<table class="main-table">
 <tr>
 	<td colspan="2">
 		<hr>
@@ -26,9 +29,9 @@
 					<?php
 						include("fos-db-connection.php");
 
-						$emailID = $_GET['user'];
+						$emailID = $_SESSION['emailID'];
 
-						$sql = "SELECT * FROM users_tab WHERE emailID = '".$emailID."'"; //this is susceptible to SQL injection, but i forgot how to do safe queries
+						$sql = "SELECT * FROM users_tab WHERE emailID = '".$emailID."'";
 						$result = $connect->query($sql);
 						$row = $result->fetch_assoc();
 						
@@ -37,18 +40,48 @@
 							<h3>Birthday: ".$row['dob']."</h3>
 							<h3>Gender: ".$row['gender']."</h3>
 							<br>";
-					?>
+							
+?>
 	</td>
-	<td colspan="2" class = "tdMain">
-		<h1>Update Details</h1>
-		<br>
-		<h3>Update Email: <input type = "text" name = "updateEmail" id = "updateEmail"/></h3>
-		<h3>Update Phone Number: <input type = "text" name = "updatePhone" id = "updatePhone"/></h3>
-		<h3>Update Birthday: <input type = "date" name = "updateBirthday" id = "updateBirthday"/></h3>
-		<h3>Update Info: <a href = "detailsProcess.php" class ="drp"><button style = "background-color: #ff6347;" name = "update" id = "update">Update</button></a>
+	<td colspan='2' class = 'tdMain'>
+<?php
+						echo "<h1>Update Details</h1>
+							<br>
+							<h3>Update First Name: <input type = 'text' name = 'updateFName' id = 'updateFName' value='".$row['firstname']."'/></h3>
+							<h3>Update Last Name: <input type = 'text' name = 'updateLName' id = 'updateLName' value='".$row['lastname']."'/></h3>
+							<h3>Update Email: <input type = 'text' name = 'updateEmail' id = 'updateEmail' value='".$emailID."'/></h3>
+							<h3>Update Password: <input type='password' ng-model='pwd' ng-change='checkPwd()' name='updatePwd' id='updatePwd' value='".$row['pwd']."'/></h3>
+							<h3>Confirm Password: <input type='password' ng-model='confirm' ng-change='checkPwd()' name='confirm' id='confirm' value='".$row['pwd']."'/><br/> {{match}} </h3>
+							<h3>Update Birthday: <input type = 'date' name = 'updateDob' id = 'updateDob' value='".$row['dob']."'/></h3>
+							<h3>Update Gender: <select id = 'updateGender' name = 'updateGender'/>
+								<option value = '".$row['gender']."' selected hidden>".$row['gender']."</option> 
+								<option value = 'Male'>Male</option>
+								<option value = 'Female'>Female</option>
+							</select>
+							<h3><a href = 'detailsProcess.php' class ='drp'><button style = 'background-color: #ff6347' name = 'update' id = 'update'>Update</button></a>";
+	?>
 	</td>
 </tr>
 </table>
+</form>
+</div>
+<script>
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+  $scope.pwd = "";
+  $scope.confirm = "";
+  $scope.match = "";
+  $scope.checkPwd = function() {
+	  if(angular.equals($scope.pwd, $scope.confirm)){
+		  $scope.match = "";
+		  document.getElementById("update").disabled = false;
+	  } else {
+		  $scope.match = "Passwords don't match!";
+		  document.getElementById("update").disabled = true;
+	  }
+  }
+});
+</script>
 </body>
 </center>
 
