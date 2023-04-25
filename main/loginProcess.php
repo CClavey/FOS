063954b4a926651a5e586d1f717496c7c8bb830e
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-04-25 01:26:32 -0500

    Update loginProcess.php

--- main/loginProcess.php
+++ main/loginProcess.php
@@ -6,23 +6,24 @@
 
 <body class = "mainBody">
 <center>
-	<a href = "home.php" class = "drp"><button style="width: 200px; height: 65px; font-size: 24px;" name = "return" id = "return">Return</button></a>
-
 	<?php
+		session_start();
 		include("fos-db-connection.php");
 
 		$emailID = $_POST['emailID'];
 		$password = $_POST['pwd'];
 
-		$sql = "SELECT pwd FROM users_tab WHERE emailID = '".$emailID."'"; //this is susceptible to SQL injection, but i forgot how to do safe queries
+		$sql = "SELECT pwd FROM users_tab WHERE emailID = '".$emailID."'";
 		$result = $connect->query($sql);
 		$pwd = mysqli_fetch_assoc($result);
 
 		if($password == $pwd['pwd']) {
-			echo "<br><br><br><center>Login Successful</center>
-			<script>
-			localStorage.setItem('email', '$emailID');
-			</script>";
+			if($emailID == "fos.admin@gmail.com")
+				echo "<a href = 'adminOrders.php' class = 'drp'><button style='width: 200px; height: 65px; font-size: 24px;' name = 'return' id = 'return'>Admin Page</button></a>";
+			else
+				echo "<a href = 'home.php' class = 'drp'><button style='width: 200px; height: 65px; font-size: 24px;' name = 'return' id = 'return'>HomePage</button></a>";
+			echo "<br><br><br><center>Login Successful</center>";
+			$_SESSION['emailID'] = $emailID;
 		} else {
 			header("Location: login.php?login=fail");
 			exit();
