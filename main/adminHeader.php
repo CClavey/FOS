52a5cf0b369a88327c0f7fb57d0e1e323ad56cbc
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-04-25 10:06:59 -0500

    Update adminHeader.php

--- main/adminHeader.php
+++ main/adminHeader.php
@@ -3,9 +3,9 @@
 <link rel="stylesheet" href="CSS/styles.css">
 <script src="JS/scripts.js"></script>
 <center>
-	<ul style="background-color:#f13e36;float:right">
+	<ul style="background-color:#f13e36">
 		
-		<!--<li class="menu-item"><a href="adminDeals.php" class="drp">Deals</a>
+		<li class="menu-item"><a href="adminDeals.php" class="drp">Deals</a>
 		</li>
 		
 		<li class="menu-item"><a href="adminPizzas.php" class="drp">Pizzas</a>
@@ -15,7 +15,7 @@
 		</li>
 		
 		<li class="menu-item"><a href="adminOrders.php" class="drp">Orders</a>
-		</li>-->
+		</li>
 		
 		<li class='menu-item'><a href='index.php' class='drp'>Logout<img src = 'Images/logout.png' height = '20px' width = '20px'></a>
 					</li>
