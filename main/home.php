a435699b9a42feed7875c66123bc002cec22e964
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-04-25 01:25:49 -0500

    Update home.php

--- main/home.php
+++ main/home.php
@@ -1,7 +1,9 @@
 <html>
 <header>
-<?php include ("header.php") ?>
-<?php include ("orderHeader.php")?>
+<?php
+include ("header.php");
+include ("orderHeader.php");
+?>
 <link rel="stylesheet" href="CSS/styles.css">
 <script src="scripts.js"></script>
 </header>
