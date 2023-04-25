6abd57a0f8daced347e6eb4dd30356a06a3bb5bc
BrownEvan189 <123338562+BrownEvan189@users.noreply.github.com>
2023-04-25 01:25:17 -0500

    Update detailsProcess.php

--- main/detailsProcess.php
+++ main/detailsProcess.php
@@ -7,7 +7,30 @@
 
 <body>
 <center>
-	<a href = "home.php" class = "drp"><button style="width: 200px; height: 65px; font-size: 24px;" name = "return" id = "return">Return</button></a>
+	<a href = "home.php" class = "drp"><button style="width: 200px; height: 65px; font-size: 24px;" name = "return" id = "return">Homepage</button></a>
+	
+	<?php
+		include("fos-db-connection.php");
+
+		$emailID = $_POST['updateEmail'];
+		$pwd = $_POST['updatePwd'];
+		if($pwd == null)
+			$pwd = $connect->query("SELECT pwd FROM users_tab WHERE emailID = '".$emailID."'")->fetch_object()->pwd;
+		$firstname = $_POST['updateFName'];
+		$lastname = $_POST['updateLName'];
+		$gender = $_POST['updateGender'];
+		$dob = $_POST['updateDob'];
+
+		$sql = "UPDATE users_tab SET emailID = '$emailID', firstname = '$firstname', lastname = '$lastname', gender = '$gender', pwd = '$pwd', dob = '$dob' WHERE emailID = '$emailID'";
+		$result = $connect->query($sql);
+
+		if($result == FALSE){
+			echo "error:".$connect->error;
+		} else {
+			echo "<br><br><br><center>Account successfully updated</center>";
+			$_SESSION['emailID'] = $emailID;
+		}
+	?>
 </center>
 </body>
 </html>
